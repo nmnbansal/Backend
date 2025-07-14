5bc6e1d9ce1e69cb82ead8c3349264aa057c91e6
bansalnmn <bansalnmn@gmail.com>
2025-07-15 00:15:22 +0530

    update cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use(express.json());
+app.use(cors());
 
 let queue = [];
 let currentServing = null;
